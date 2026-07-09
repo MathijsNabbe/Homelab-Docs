53cc2eb8072af72310991b1db607185944a00cb7
MathijsNabbe <datenenaampje@gmail.com>
2026-07-09 17:28:24 +0200

    Simplify `servicesDirectory` path resolution in `Program.cs`.

--- Parser/src/Parser/Program.cs
+++ Parser/src/Parser/Program.cs
@@ -1,7 +1,6 @@
 ﻿using Parser.Helpers;
 
-var repoRoot = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../.."));
-var servicesDirectory = Path.Combine(repoRoot, "Services");
+var servicesDirectory = Path.GetFullPath(Path.Combine(AppContext.BaseDirectory, "../../../../../../Services"));
 
 var composeFiles = Directory.EnumerateFiles(servicesDirectory, "*", SearchOption.AllDirectories)
     .Where(file => RegexHelper.IsComposeFile(Path.GetFileName(file)));
